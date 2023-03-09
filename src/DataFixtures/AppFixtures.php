ee47703eda5eb7a1f9eb787a6be0e53bd5fd5796
estephant2022 <emilie.stephant2022@campus-eni.fr>
2023-03-09 14:06:01 +0100

    Tableau d'affichage des sorties bien avancé, on retouche aux fixtures ENCOOOOOOOOOOOORE

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -61,12 +61,12 @@ class AppFixtures extends Fixture
 
         $sortie->setNom($this->faker->name);
         $sortie->setInfosSortie($this->faker->name);
-        $sortie->setDuree($this->faker->randomNumber());
+        $sortie->setDuree($this->faker->randomNumber(3));
         $sortie->setDateHeureDebut($this->faker->dateTimeBetween('-2years', 'now'));
-        $sortie->setDateLimiteInscription($sortie->getDateHeureDebut());
+        $sortie->setDateLimiteInscription($this->faker->dateTimeBetween('-2years', 'now'));
         $sortie->setEtat($this->faker->randomElement($etats));
         $sortie->setLieu($this->faker->randomElement($lieux));
-        $sortie->setNbInscriptionsMax($this->faker->randomNumber());
+        $sortie->setNbInscriptionsMax($this->faker->randomNumber(3));
         $sortie->setSiteOrganisateur($this->faker->randomElement($campus));
         $sortie->setOrganisateur($this->faker->randomElement($users));
 
